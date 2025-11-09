ed8e5d1afc0a597b0bb6e8db10248b2de4680bb1
Andy Pan <andy0130tw@yahoo.com.tw>
2025-11-09 18:26:40 +0800

    Fix a bug where memfsUnzip cannot extract empty files

--- zip-utils.ts
+++ zip-utils.ts
@@ -21,13 +21,15 @@ export async function memfsUnzip(memfs: MemoryFileSystem, data: Uint8Array, opti
     if (file.dir) {
       memfs.createDirectory(path)
     } else {
+      const data = (file as any)._data
       // https://github.com/Stuk/jszip/issues/643
-      const { uncompressedSize } = (file as any)._data as { uncompressedSize: number }
-
-      memfs.createFile(path, {
-        size: BigInt(uncompressedSize),
+      // XXX: there is a quirk that a zero-sized file is converted to a promise;
+      // losing its uncompressed size
+      const actualSize: number = data?.uncompressedSize ?? 0
+      memfs.createFile(path, actualSize !== 0 ? {
+        size: BigInt(actualSize),
         reader: () => file.async('uint8array'),
-      })
+      } : new Uint8Array())
     }
   })
 
@@ -50,17 +52,12 @@ export function memfsUnzipPrim(
     const lastSlash = relpath.lastIndexOf('/')
     const dirname = lastSlash >= 0 ? relpath.slice(0, lastSlash) : ''
 
-    let skipped = false
-
-    // if a dir is ignored, all its children are skipped
-    // if jszip always iterates in pre-order, we can do better by using a stack
-    if (dirname && ignoredDirs.has(dirname)) {
-      skipped = true
-    }
-
-    if (!skipped && filter && !filter(relpathOrig)) {
-      skipped = true
-    }
+    const skipped = (
+      // if a dir is ignored, all its children are skipped
+      // if jszip always iterates in pre-order, we can do better by using a stack
+      (dirname && ignoredDirs.has(dirname)) ||
+      // if not, we query the filter function to find out
+      (filter && !filter(relpathOrig)))
 
     if (skipped) {
       if (file.dir) {
